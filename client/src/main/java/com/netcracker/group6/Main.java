f0f8fe971e582359ea90486ce0f4829341e04aab
darthMilash <dani4eg92@gmail.com>
2017-06-07 12:17:36 +0300

    create project. add modules

---
+++ client/src/main/java/com/netcracker/group6/Main.java
@@ -0,0 +1,11 @@
+package com.netcracker.group6;
+
+/**
+ * Created by darthMilash on 07.06.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello client");
+    }
+}
