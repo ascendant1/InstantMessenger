cba34abbda6168c0838a4dbd5e0672e8351e1498
darthMilash <dani4eg92@gmail.com>
2017-06-07 14:48:01 +0300

    Add: 1. javafx-maven-plugin 2. base model RootWindow.fxml 3. class-controller RootWindowController 4. css DarkTheme

--- client/src/main/java/com/netcracker/group6/Main.java
+++ client/src/main/java/com/netcracker/group6/Main.java
@@ -1,16 +1,54 @@
 package com.netcracker.group6;
 
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 /**
  * Created by darthMilash on 07.06.2017.
  */
-public class Main {
+public class Main extends Application {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class);
+    private Stage primaryStage;
+    private BorderPane rootLayout;
 
     public static void main(String[] args) {
-        System.out.println("Hello client");
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Instant Messenger");
+
+        initRootWindow();
+
+//        this.primaryStage.getIcons().add(new Image("/images/icon.png"));
+
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void initRootWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/fxml/RootWindow.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setResizable(false);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            LOGGER.error("RootWindow data access error");
+        }
     }
 }
---
+++ client/src/main/java/com/netcracker/group6/controller/RootWindowController.java
@@ -0,0 +1,28 @@
+package com.netcracker.group6.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
+/**
+ * Created by darthMilash on 07.06.2017.
+ */
+public class RootWindowController   {
+
+    @FXML
+    public void handleAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Instant Messenger");
+        alert.setHeaderText("About");
+        alert.setContentText("Authors: \n2017");
+
+        alert.showAndWait();
+    }
+
+    /**
+     * Close app
+     */
+    @FXML
+    public void handleExit() {
+        System.exit(0);
+    }
+}
