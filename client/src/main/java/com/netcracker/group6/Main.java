17ad2529114fec982632899118b03093ed685915
darthMilash <dani4eg92@gmail.com>
2017-06-07 12:41:32 +0300

    add  Apache Log4j dependency in client and server

--- client/src/main/java/com/netcracker/group6/Main.java
+++ client/src/main/java/com/netcracker/group6/Main.java
@@ -1,9 +1,14 @@
 package com.netcracker.group6;
 
+
+import org.apache.log4j.Logger;
+
 /**
  * Created by darthMilash on 07.06.2017.
  */
 public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 
     public static void main(String[] args) {
         System.out.println("Hello client");
